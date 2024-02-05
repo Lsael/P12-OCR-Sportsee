3ad721cd4816f524435b93d01046ba907064aea1
Lsael <l.saelens@proton.me>
2024-02-05 21:44:12 +0100

    Added recharts on all graph and totally removed Chartjs

--- src/components/Dashboard/graphs/Intensity/Intensity.js
+++ src/components/Dashboard/graphs/Intensity/Intensity.js
@@ -1,93 +1,33 @@
 import React from "react";
 import styles from "./Intensity.module.scss";
-import {
-  Chart as ChartJS,
-  RadialLinearScale,
-  PointElement,
-  LineElement,
-  Filler,
-  Tooltip,
-  Legend,
-} from "chart.js";
-import { Radar } from "react-chartjs-2";
-
-ChartJS.register(
-  RadialLinearScale,
-  PointElement,
-  LineElement,
-  Filler,
-  Tooltip,
-  Legend
-);
+import { Legend, PolarAngleAxis, PolarGrid, PolarRadiusAxis, Radar, RadarChart } from "recharts";
 
 const Intensity = (props) => {
-  const options = {
-    responsive: true,
-    scales: {
-      r: {
-        grid: {
-          color: "white",
-        },
-        ticks: {
-          display: false,
-          stepSize: 50,
-        },
-        suggestedMin: 0,
-        pointLabels: {
-          color: "white",
-          font: {
-            size: 12,
-          },
-        },
-      },
-    },
-    elements: {
-      point: {
-        backgroundColor: "#FFFFFF",
-        radius: 0,
-        hitRadius: 50,
-        hoverRadius: 5,
-      },
-    },
-    plugins: {
-      legend: {
-        display: false,
-      },
-      title: {
-        display: false,
-      },
-      tooltip: {
-        backgroundColor: "#FFFFFF",
-        titleColor: "black",
-        bodyColor: "black",
-        bodyFont: {
-          weight: "bold",
-        },
-        padding: 10,
-        cornerRadius: 0,
-        displayColors: false,
-        callbacks: {
-          title: () => {
-            return null;
-          },
-        },
-      },
-    },
-  };
-
-  const data = {
-    labels: props.intensity.kinds,
-    datasets: [
-      {
-        data: props.intensity.datas,
-        backgroundColor: "rgba(255, 1, 1, 0.7)",
-      },
-    ],
-  };
+  console.log(props);
+  const data = {}
 
   return (
     <div className={styles.Intensity}>
-      <Radar options={options} data={data} />
+      <RadarChart outerRadius={90} width={730} height={250} data={data}>
+        <PolarGrid />
+        <PolarAngleAxis dataKey="subject" />
+        <PolarRadiusAxis angle={30} domain={[0, 150]} />
+        <Radar
+          name="Mike"
+          dataKey="A"
+          stroke="#8884d8"
+          fill="#8884d8"
+          fillOpacity={0.6}
+        />
+        <Radar
+          name="Lily"
+          dataKey="B"
+          stroke="#82ca9d"
+          fill="#82ca9d"
+          fillOpacity={0.6}
+        />
+        <Legend />
+      </RadarChart>
     </div>
   );
 };
--- src/components/Dashboard/graphs/Score/Score.js
+++ src/components/Dashboard/graphs/Score/Score.js
@@ -1,51 +1,33 @@
 import React from "react";
 import styles from "./Score.module.scss";
-import { Chart as ChartJS, ArcElement, Tooltip, Legend } from "chart.js";
-import { Doughnut } from "react-chartjs-2";
-
-ChartJS.register(ArcElement, Tooltip, Legend);
+import { Pie, PieChart } from "recharts";
 
 const Score = (props) => {
-  const options = {
-    cutout: "85%",
-    responsive: true,
-    borderColor: "#FBFBFB",
-    plugins: {
-      legend: {
-        display: false
-      },
-      title: {
-        display: true,
-        text: "Score",
-        align: "start",
-        color: "black",
-        font: {
-          weight: "bold",
-          size: "18",
-        }
-      },
-    },
-    // disable Hover and Tooltips
-    events: []
-  };
-
-  const data = {
-    labels: [],
-    datasets: [
-      {
-        label: "%",
-        data: [1 - props.score, props.score],
-        backgroundColor: ["#FBFBFB", "rgba(255, 1, 1)"],
-        borderRadius: 1000
-      },
-    ],
-  };
-
+  const data = [{
+    name: "score",
+    score: props.score
+  },
+  {
+    name: "placeholder",
+    score: 1 - props.score
+  }]
   return (
     <div className={styles.Score}>
-      <Doughnut options={options} data={data} />
+      <PieChart width={730} height={250}>
+        <Pie
+          data={data}
+          nameKey="name"
+          dataKey="score"
+          cx="50%"
+          cy="50%"
+          outerRadius={50}
+          fill="rgba(255, 1, 1)"
+        />
+      </PieChart>
       <p className={styles.Score__count}>
-        <span className={styles.Score__percentage}>{`${props.score * 100} %`}</span>
+        <span className={styles.Score__percentage}>{`${
+          props.score * 100
+        } %`}</span>
         <span className={styles.Score__span}>de votre objectif</span>
       </p>
     </div>
