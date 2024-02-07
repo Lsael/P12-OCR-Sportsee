e36e300dbc58474aee95156bd23bf38b636595db
Lsael <l.saelens@proton.me>
2024-02-07 14:58:16 +0100

    Did all DailyActivity style

--- src/components/Dashboard/graphs/DailyActivity/DailyActivity.js
+++ src/components/Dashboard/graphs/DailyActivity/DailyActivity.js
@@ -11,20 +11,52 @@ import {
   ResponsiveContainer,
 } from "recharts";
 
+const CustomLegend = () => {
+  return (
+    <div className={styles.DailyActivity__head}>
+      <h2>Activité quotidienne</h2>
+      <div className={styles.DailyActivity__legend}>
+        <p className={styles.weight}>
+          <span>&#9679;</span>
+          <span>Poids (kg)</span>
+        </p>
+        <p className={styles.calories}>
+          <span>&#9679;</span>
+          <span>Calories brûlées (kCal)</span>
+        </p>
+      </div>
+    </div>
+  );
+};
+
 const DailyActivity = (props) => {
   const data = props.dailyActivity;
 
   return (
     <div className={`${styles.DailyActivity} graph`}>
       <ResponsiveContainer width="100%" height="100%">
-        <BarChart width={850} height={250} data={data}>
-          <CartesianGrid strokeDasharray="3 3" />
-          <XAxis dataKey="name" />
-          <YAxis />
-          <Tooltip />
-          <Legend />
-          <Bar dataKey="kilogram" fill="#282D30" />
-          <Bar dataKey="calories" fill="#E60000" />
+        <BarChart
+          width={850}
+          height={250}
+          data={data}
+          barCategoryGap={30}
+          barGap={16}
+          margin={{ top: 10, right: 0, bottom: 20, left: 10 }}
+        >
+          <CartesianGrid strokeDasharray="3" vertical={false} />
+          <XAxis dataKey="name" tickMargin={20} tickLine={false} stroke="#9B9EAC"/>
+          <YAxis orientation="right" axisLine={false} tickMargin={20} tickLine={false} stroke="#9B9EAC"/>
+          <Tooltip
+            separator=""
+            wrapperStyle={{ top: "-20%", left: 50 }}
+            contentStyle={{ background: "#E60000" }}
+            labelStyle={{ display: "none" }}
+            itemStyle={{ color: "white", lineHeight: "3" }}
+            formatter={(name, value, unit) => [name, unit]}
+          />
+          <Legend content={<CustomLegend />} verticalAlign="top" height="20%"/>
+          <Bar dataKey="kilogram" fill="#282D30" unit="kg" radius={[50, 50, 0, 0]}/>
+          <Bar dataKey="calories" fill="#E60000" unit="kCal" radius={[50, 50, 0, 0]}/>
         </BarChart>
       </ResponsiveContainer>
     </div>
