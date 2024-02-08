eb0170ec4e403d8b9f8c131269e967e6e767310e
Lsael <l.saelens@proton.me>
2024-02-08 12:07:24 +0100

    Improved AverageSession style and building hover effect

--- src/components/Dashboard/graphs/AverageSession/AverageSession.js
+++ src/components/Dashboard/graphs/AverageSession/AverageSession.js
@@ -14,17 +14,15 @@ const CustomLegend = () => {
   return <h3>Durée moyenne des sessions</h3>;
 };
 
-const CustomTicks = ({labels}) => {
-  return(
+const CustomTicks = ({ labels }) => {
+  return (
     <p className={styles.AverageSession__customTicks}>
-    {labels.map((e) => {
-      return(
-        <span>{e}</span>
-      )
-    })}
+      {labels.map((e) => {
+        return <span>{e}</span>;
+      })}
     </p>
-  )
-}
+  );
+};
 
 const AverageSession = (props) => {
   const labels = ["L", "M", "M", "J", "V", "S", "D"];
@@ -42,22 +40,36 @@ const AverageSession = (props) => {
         <LineChart
           data={data}
           margin={{ top: 30, right: 0, left: 0, bottom: 50 }}
+          onMouseMove={(e) => {
+            console.log(e);
+              document.querySelector(`.${styles.AverageSession__customHover}`).style.left = e.chartX
+            }
+          }
         >
-          <XAxis
-            dataKey="name"
-            hide={true}
+          <XAxis dataKey="name" hide={true} />
+          <Tooltip
+            contentStyle={{width: "fit-content"}}
+            itemStyle={{ color: "black" }}
+            allowEscapeViewBox={{ x: true, y: true }}
+            labelStyle={{display: "none"}}
+            formatter={(value, name) => [value]}
           />
-          <Tooltip />
           <Legend
             verticalAlign="top"
             height="20%"
             width="80%"
             content={<CustomLegend />}
           />
-          <Line type="basis" dataKey="sessionData" stroke="white" dot={false}  />
+          <Line
+            type="natural"
+            dataKey="sessionData"
+            stroke="white"
+            dot={false}
+          />
         </LineChart>
       </ResponsiveContainer>
       <CustomTicks labels={labels} />
+      <p className={styles.AverageSession__customHover}></p>
     </div>
   );
 };
