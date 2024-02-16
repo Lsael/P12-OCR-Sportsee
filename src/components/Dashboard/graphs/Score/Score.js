03d0e503da523085ecceb0eaf8626246e62056d4
Lsael <l.saelens@proton.me>
2024-02-16 14:18:20 +0100

    Improved Score graph styles

--- src/components/Dashboard/graphs/Score/Score.js
+++ src/components/Dashboard/graphs/Score/Score.js
@@ -1,6 +1,18 @@
 import React from "react";
 import styles from "./Score.module.scss";
-import { Cell, Pie, PieChart, ResponsiveContainer } from "recharts";
+import { Cell, Label, Pie, PieChart, ResponsiveContainer } from "recharts";
+
+const CustomLabel = (props) => {
+  console.log(props.value);
+  return (
+    <p className={styles.Score__count}>
+      <span className={styles.Score__percentage}>{`${
+        props.value * 100
+      } %`}</span>
+      <span className={styles.Score__span}>de votre objectif</span>
+    </p>
+  );
+};
 
 const Score = (props) => {
   const data = [
@@ -11,10 +23,17 @@ const Score = (props) => {
     {
       name: "placeholder",
       score: 1 - props.score,
-    },
+    }
   ];
-  
-  const COLORS = ['rgba(255, 1, 1)', 'white'];
+
+  const placeholder = [
+    {
+      name: "placeholder2",
+      value: 1
+    }
+  ]
+
+  const COLORS = ["rgba(255, 1, 1)", "#FBFBFB"];
 
   return (
     <div className={styles.Score}>
@@ -29,14 +48,15 @@ const Score = (props) => {
             cy="50%"
             outerRadius={85}
             innerRadius={70}
-            fill="rgba(255, 1, 1)"
             cornerRadius={50}
             startAngle={-270}
-            >
-            {
-              data.map((value, index) => <Cell key={index} fill={COLORS[index % COLORS.length]}/>)
-            }
-            </Pie>
+            stroke="#FBFBFB"
+          >
+            {data.map((value, index) => (
+              <Cell key={index} fill={COLORS[index % COLORS.length]} />
+            ))}
+          </Pie>
+          <Pie data={placeholder} dataKey="value" cx="50%" cy="50%" outerRadius={70} fill="white" />
         </PieChart>
         <p className={styles.Score__count}>
           <span className={styles.Score__percentage}>{`${
